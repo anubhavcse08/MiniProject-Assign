b7714ce1bb1cc4a253e165f3003f62b985c9ac1a
Anubhav kumar <anubhavcse08@users.noreply.github.com>
2018-12-16 22:39:37 +0530

    Add files via upload

---
+++ Youtube Api ReactRedux/src/store/reducer/reducerList.js
@@ -0,0 +1,10 @@
+
+export default function(state={}, action) {
+    switch (action.type) {
+        case 'YOUTUBE_API':
+            return {...state, videoData:action.payload}
+                
+        default:
+            return state
+    }
+}
